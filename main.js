cf6ec8cfb15fa282d6af9e3182124a701fd67ebc
rrdmp15 <rdayismar@gmail.com>
2023-03-07 12:41:55 -0500

    feat: :tada: Metodo charAt
    Este metodo permite mostrar el caracter en el orden especificado de una string

---
+++ main.js
@@ -0,0 +1,8 @@
+let metodoChartAt = "Este es el primer string"
+// ``
+console.log("El caracter en el indice 19 es '" + metodoChartAt.charAt(18) + "'")
+console.log("El caracter en el indice 19 es '" + metodoChartAt.charAt(19) + "'")
+console.log("El caracter en el indice 19 es '" + metodoChartAt.charAt(20) + "'")
+console.log("El caracter en el indice 19 es '" + metodoChartAt.charAt(21) + "'")
+console.log("El caracter en el indice 19 es '" + metodoChartAt.charAt(22) + "'")
+console.log("El caracter en el indice 19 es '" + metodoChartAt.charAt(23) + "'")
