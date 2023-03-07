60b5c0c5fcca0d71b425bb7ba484acc53b14baf4
rrdmp15 <rdayismar@gmail.com>
2023-03-07 12:50:23 -0500

    feat: :rocket: Metodo charCodeAt
    Este devuelve el valor unicode del indice indicado dentro de la string, en este caso se la string unicode como ejemplo

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-let metodoChartAt = "Este es el primer string"
+let metodoCharAt = "Este es el primer string";
 // ``
-console.log("El caracter en el indice 19 es '" + metodoChartAt.charAt(18) + "'")
-console.log("El caracter en el indice 19 es '" + metodoChartAt.charAt(19) + "'")
-console.log("El caracter en el indice 19 es '" + metodoChartAt.charAt(20) + "'")
-console.log("El caracter en el indice 19 es '" + metodoChartAt.charAt(21) + "'")
-console.log("El caracter en el indice 19 es '" + metodoChartAt.charAt(22) + "'")
-console.log("El caracter en el indice 19 es '" + metodoChartAt.charAt(23) + "'")
+console.log("El caracter en el indice 19 es '" + metodoCharAt.charAt(18) + "'")
+console.log("El caracter en el indice 20 es '" + metodoCharAt.charAt(20) + "'")
+console.log("El caracter en el indice 21 es '" + metodoCharAt.charAt(21) + "'")
+console.log("El caracter en el indice 22 es '" + metodoCharAt.charAt(22) + "'")
+console.log("El caracter en el indice 23 es '" + metodoCharAt.charAt(23) + "'")
+console.log("El caracter en el indice 24 es '" + metodoCharAt.charAt(24) + "'")
+
+let metodocharCodeAt = "Unicode"
+
+console.log("El valor Unicode de 'U' es '" + metodocharCodeAt.charCodeAt(0) + "'")
