6c0e10df32c739d332f7a31fc77bb199731dbe7f
rrdmp15 <rdayismar@gmail.com>
2023-03-08 13:10:05 -0500

    feat: :rocket: Operador OR
    devuelve el primer valor si alguno de los dos valores es true

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-let verdadero = true && true;
-let falso = true && false;
-let falso1 = false && true;
-let falso2 = false && false;
+let verdadero = true || true;
+let falso = true || false;
+let falso1 = false || true;
+let falso2 = false || false;
 
 console.log(verdadero);
 console.log(falso);
