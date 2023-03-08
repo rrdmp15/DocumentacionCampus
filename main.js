f0c1b19d83d40b2b214abd6a4d7d8beebca1e6e8
rrdmp15 <rdayismar@gmail.com>
2023-03-08 13:01:46 -0500

    feat: :rocket: Devuelve true si el primer valor es menor o igual al segundo valor

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let numero1 = 10
 let numero2 = 5
 
-console.log(numero1 >= numero2)
-console.log(numero1 >= numero1)
-console.log(numero2 >= numero1)
+console.log(numero1 <= numero2)
+console.log(numero2 <= numero2)
+console.log(numero2 <= numero1)
