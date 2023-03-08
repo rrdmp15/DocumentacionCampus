b621b10100208e6ee9824c590ca80c6c43f40d49
rrdmp15 <rdayismar@gmail.com>
2023-03-08 13:08:47 -0500

    feat: :rocket: Operador AND
    este operador devuelve el primer valor solo si ambos valores son true

---
+++ main.js
@@ -0,0 +1,9 @@
+let verdadero = true && true;
+let falso = true && false;
+let falso1 = false && true;
+let falso2 = false && false;
+
+console.log(verdadero);
+console.log(falso);
+console.log(falso1);
+console.log(falso2);
