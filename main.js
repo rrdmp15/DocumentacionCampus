91bf59e3fcf26669be79d7b10510c7cf7bb95d07
rrdmp15 <rdayismar@gmail.com>
2023-03-08 13:24:37 -0500

    docs: :sparkles: Operadores Aritmeticos
    A partir de aca se empezaran a documentar los operadores aritmeticos

--- main.js
+++
@@ -1,9 +0,0 @@
-let falso = true ^ true;
-let falso1 = false ^ false;
-let verdadero = true ^ false;
-let verdadero1 = false ^ true;
-
-console.log(verdadero);
-console.log(falso);
-console.log(verdadero1);
-console.log(falso1);
