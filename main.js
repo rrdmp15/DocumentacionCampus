5d451d635a5279ea575bd6fb29fb80b5133d70e3
rrdmp15 <rdayismar@gmail.com>
2023-03-08 13:23:46 -0500

    feat: :rocket: Operador  XOR
    devuelve true solo si uno de los dos valores es true pero no ambos

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-let verdadero = !true;
-let falso = !false;
-
+let falso = true ^ true;
+let falso1 = false ^ false;
+let verdadero = true ^ false;
+let verdadero1 = false ^ true;
 
 console.log(verdadero);
 console.log(falso);
+console.log(verdadero1);
+console.log(falso1);
