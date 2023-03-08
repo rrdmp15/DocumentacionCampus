ab13d96215a551439b7871b7c92961738357582b
rrdmp15 <rdayismar@gmail.com>
2023-03-08 12:57:10 -0500

    feat: :rocket: Operador Mayor que
    este devuelve true si el primer valor es mayor que el valor siguiente

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-let noIgualito = "4"
+let numero1 = 10
+let numero2 = 5
 
-console.log(noIgualito !== 4)
-console.log(noIgualito !== "3")
-console.log(noIgualito !== "4")
-
+console.log(numero2 > numero1)
+console.log(numero1 > numero2)
