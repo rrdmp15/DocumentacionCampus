5743036b6d25eca38593c18e4782af802f936fc4
rrdmp15 <rdayismar@gmail.com>
2023-04-18 08:51:15 -0500

    feat: :rocket: Promesas
    Las promesas son objetos que representan la terminacion o el fracaso de una operacion asincronica. en el ejemplo se crea una constante llamada obtenerResultado que usa una funcion flecha para crear una nueva promesa donde se simula una operacion asincronica definiendo un setTimeout de 3s donde se define la constante resultado que con un math random se le pregunta si es mayor que 0.5, si es mayor devuelve exito y sino fracaso. luego se usa la promesa mostrando que si la tarea se realizo, muestra en consola si fue con exito o fracaso. y si no se pudo realizar la tarea solo muestra un error en consola

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-document.querySelector("#btn").addEventListener("click", ()=>{
-    alert("hiciste click en un boton");
-})
+const obtenerResultado = ()=>{
+    return new Promise((resolve, reject)=>{
+        setTimeout( ()=>{
+        const resultado = Math.random() > 0.5 ? 'Exito' : 'Fracaso';
+        resolve(resultado);
+        }, 3000);
+    })
+}
+
+obtenerResultado()
+  .then((resultado) => {
+    console.log(`La tarea se completó con ${resultado}`);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
